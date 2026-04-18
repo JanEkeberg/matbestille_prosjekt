8b1d0c6c932033bfc9074da19abf82cd21eed3f7
JanEkeberg <Ekpar@Ramiles-Mac-Studio.local>
2026-04-18 23:33:17 +0200

    la til kode i IRepository interface

--- Interfaces/IRepository.cs
+++ Interfaces/IRepository.cs
@@ -2,5 +2,19 @@ namespace DefaultNamespace;
 
 public interface IRepository
 {
+    // hent alle objekter
+    List<t> GetAll();
     
+    //hent et objekt via ID
+    T? GetById(string id);
+    
+    //legg til et nytt objekt
+    void Add(T item);
+    
+    //oppdater eksisterende objekt
+    void Update(T item);
+    
+    //slett objekt via iD 
+    void Delete(string id); 
+
 }
