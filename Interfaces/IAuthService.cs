fc525d203b7bc7464ea9408d1f816389ff801269
JanEkeberg <Ekpar@Ramiles-Mac-Studio.local>
2026-04-19 00:18:41 +0200

    la til kode i IAuthService interface

--- Interfaces/IAuthService.cs
+++ Interfaces/IAuthService.cs
@@ -2,5 +2,13 @@ namespace DefaultNamespace;
 
 public interface IAuthService
 {
+    //logg inn _returne bruker eller null
+    Use? Login(string email, string password);
+
+    //registere ny kunder
+    Customer Register(string name, string surname, string email, string password, string tekNumber, string orgNumber,
+        string teamNumber);
     
+    //sjekk om e-post allerede er registert
+    bool EmailExists(string email);
 }
