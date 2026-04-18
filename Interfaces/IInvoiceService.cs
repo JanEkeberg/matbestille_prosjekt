3792df85030d27cc5c682dc36a7133b5f4933e64
JanEkeberg <Ekpar@Ramiles-Mac-Studio.local>
2026-04-19 00:16:11 +0200

    la til kode i IInvoiceService interface

--- Interfaces/IInvoiceService.cs
+++ Interfaces/IInvoiceService.cs
@@ -2,5 +2,12 @@ namespace DefaultNamespace;
 
 public interface IInvoiceService
 {
+    //opprett faktura fra en bestilling
+    Invoice GenerateInvoice(string orderId);
+    //hent alle faktura
+    List<Invoice> GetAllInvoices();
+    //sender faktura på eposten
+    void sendInvoice(string invoiceId);
+    //skrive ut faktura
+    void PrintInvoice(Invoice invoice);
     
-}
