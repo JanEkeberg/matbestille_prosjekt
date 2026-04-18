35547609e78802e9bdd60b1527457c8ae623138d
JanEkeberg <Ekpar@Ramiles-Mac-Studio.local>
2026-04-19 00:00:57 +0200

    la til kode i IOrderService interface

--- Interfaces/IOrderService.cs
+++ Interfaces/IOrderService.cs
@@ -2,5 +2,14 @@ namespace DefaultNamespace;
 
 public interface IOrderService
 {
-    
+    //opprette en ny bestiling og lager den 
+    Order CreateOrder(string customerId, string roomNumber, DateTime deliveryTime, List<OrderLine> lines);
+    //hente alle bestiling for em kunde
+    List<Order> GetOrdersByCustomer(string customerId);
+    //hent alle bestiling fra ansatt
+    List<Order> GetAllOrders();
+    //hent senere bestilling
+    List<Order> GetUpComingOrders();
+    //merk bestiliing som har levert 
+    void MarkAsDelivered(string orderId);
 }
